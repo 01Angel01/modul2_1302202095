5958ed4f2438f25fd1a30b73ddee8dcedf4dc8f5
Angelina Yessyca Rahardjo Hadi <90541566+ComicalBanana@users.noreply.github.com>
2022-03-05 14:56:38 +0700

    Add files via upload

---
+++ modul2_1302202095/modul2_1302202095/Form1.cs
@@ -0,0 +1,166 @@
+namespace modul2_1302202095
+{
+    public partial class Form1 : Form
+    {
+        decimal bill1;
+        decimal bill2;
+        int opr;
+        Boolean opr_selesai = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "1";
+            }
+            else
+            {
+                textBox1.Text += "1";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "2";
+            }
+            else
+            {
+                textBox1.Text += "2";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "3";
+            }
+            else
+            {
+                textBox1.Text += "3";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "4";
+            }
+            else
+            {
+                textBox1.Text += "4";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "5";
+            }
+            else
+            {
+                textBox1.Text += "5";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "6";
+            }
+            else
+            {
+                textBox1.Text += "6";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "7";
+            }
+            else
+            {
+                textBox1.Text += "7";
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "8";
+            }
+            else
+            {
+                textBox1.Text += "8";
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "9";
+            }
+            else
+            {
+                textBox1.Text += "9";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text += "0";
+            }
+        }
+
+        private void buttonplus_Click(object sender, EventArgs e)
+        {
+            bill1 = Convert.ToDecimal(textBox1.Text);
+            textBox2.Text = "+";
+            textBox1.Text = " ";
+            opr = 7;
+            opr_selesai = true;
+        }
+
+        private void buttonhasil_Click(object sender, EventArgs e)
+        {
+            if (opr_selesai == true)
+                bill2 = Convert.ToDecimal(textBox1.Text);
+            {
+                switch (opr)
+                {
+                    case 1:
+                        textBox1.Text = Convert.ToString(bill1 + bill2);
+                        break;
+                }
+                opr_selesai = false;
+
+            }
+        }
+
+    }
+}
